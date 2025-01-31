19c677e21449cc085fa349553db9f9b493971c55
Josue Chavez <79125@alumnos.utleon.edu.mx>
2025-01-30 18:47:04 -0600

    layout

--- main.py
+++ main.py
@@ -1,15 +1,59 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hola mundo"
+    titulo="IDGS805"
+    lista=["Pedro,juan,Mario"]
+    return render_template("index.html",titulo=titulo,lista=lista)
+
+@app.route("/ejemplo1")
+def ejemplo1():
+    return render_template("ejemplo1.html")
+
+@app.route("/ejemplo2")
+def ejemplo2():
+    return render_template("ejemplo2.html")
+
 
 @app.route("/Hola")
 def hola():
-    return "Hola mundooo"
+    return "<h1>Hola mundooo</h1>"
+
+
+@app.route("/user/<string:user>")
+def user(user):
+    return f"hola,{user}"
+
+@app.route("/numero/<int:n>")
+def numero(n):
+    return f"El numeri es: {n}"
+
+@app.route("/user/<int:id>/<string:username>")
+def username(id,username):
+    return f"El usuario es: {username} con id: {id}"
+
+@app.route("/suma/<float:n1>/<float:n2>")
+def suma(n1, n2):
+    return f"La suma es: {n1+n2}"
+
+@app.route("/default/")
+@app.route("/default/<string:tem>")
+def func1(tem='Juan'):
+    return f"Hola, {tem}"
+
+@app.route("/form1")
+def form1():
+    return '''
+            <form>
+            <label for="nombre">Nombre:</label>
+            </form>
+    
+           '''
+
 
 if __name__=="__main__":
     app.run(debug=True,port=3000)
+
 
